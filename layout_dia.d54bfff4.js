2b13bf42be08f08f2c60b07e08df8a30e974b058
Vlad Chudin <chudintttks@gmail.com>
2026-01-12 17:27:45 +0100

    make build

---
+++ layout_dia.d54bfff4.js
@@ -0,0 +1,2 @@
+"use strict";const heroSwiper=new Swiper(".hero__photo",{loop:!0,effect:"fade",fadeEffect:{crossFade:!0},speed:1200,autoplay:{delay:3e4,disableOnInteraction:!1,pauseOnMouseEnter:!0},navigation:{nextEl:".hero__arrow--right",prevEl:".hero__arrow--left"}});
+//# sourceMappingURL=layout_dia.d54bfff4.js.map
