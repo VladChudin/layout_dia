433c8a68d60bc29de41f12f2b78b9b82f7eb6993
Vlad Chudin <chudintttks@gmail.com>
2026-01-12 17:27:13 +0100

    menu

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,23 @@
 'use strict';
+
+const heroSwiper = new Swiper('.hero__photo', {
+  loop: true,
+
+  effect: 'fade',
+  fadeEffect: {
+    crossFade: true,
+  },
+
+  speed: 1200,
+
+  autoplay: {
+    delay: 30000, // ⏱️ 30 секунд
+    disableOnInteraction: false,
+    pauseOnMouseEnter: true,
+  },
+
+  navigation: {
+    nextEl: '.hero__arrow--right',
+    prevEl: '.hero__arrow--left',
+  },
+});
